643e4d054d505d9ab5a8b62ad9990df51ce337a0
FlameRat <yxyyb@qq.com>
2019-07-26 23:11:06 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -3,16 +3,16 @@ from fabric import Connection
 
 def install_depends_by_apt(c):
     c.local(
-        'sudo apt install unzip python3.7-dev fonts-noto fonts-noto-color-emoji chromium-browser nginx redis npm git python-pip -y')
+        'apt install unzip python3.7-dev fonts-noto fonts-noto-color-emoji chromium-browser nginx redis npm git python-pip -y')
 
 
 def install_driver(c):
     c.local('wget https://chromedriver.storage.googleapis.com/75.0.3770.140/chromedriver_linux64.zip')
     c.local('unzip chromedriver_linux64.zip')
     c.local('chmod +x chromedriver')
-    c.local('sudo mv -f chromedriver /usr/local/share/chromedriver')
-    c.local('sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver')
-    c.local('sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver')
+    c.local('mv -f chromedriver /usr/local/share/chromedriver')
+    c.local('ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver')
+    c.local('ln -s /usr/local/share/chromedriver /usr/bin/chromedriver')
 
 
 def git_pull(c):
@@ -31,8 +31,8 @@ def install_depends_by_pip(c):
 
 
 def register_pm2(c):
-    c.local('sudo npm install pm2 -g')
-    c.local('sudo pm2 startup')
+    c.local('npm install pm2 -g')
+    c.local('pm2 startup')
     with c.lcd('matsuri_translation'):
         c.local('python -m pipenv run pm2 start run.sh')
         c.local('python -m pipenv run pm2 start celery_run.sh')
